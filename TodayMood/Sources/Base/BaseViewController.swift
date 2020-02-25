13264ad1ed9e876fa457f5822f0a331ff31002ee
Kanz <87kangsw@gmail.com>
2020-02-26 00:12:17 +0900

    feat(config): Base 클래스 추가

---
+++ TodayMood/Sources/Base/BaseViewController.swift
@@ -0,0 +1,68 @@
+//
+//  BaseViewController.swift
+//  TodayMood
+//
+//  Created by Kanz on 2020/02/26.
+//  Copyright © 2020 5boon. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+
+class BaseViewController: UIViewController {
+
+    lazy private(set) var className: String = {
+        return type(of: self).description().components(separatedBy: ".").last ?? ""
+    }()
+    
+    // MARK: - Properties
+    var safeAreaInsets: UIEdgeInsets {
+        guard let window = UIApplication.shared.windows.first else { return self.view.safeAreaInsets }
+        return window.safeAreaInsets
+    }
+    private(set) var didSetupConstraints = false
+
+    // MARK: - Initialize
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        self.init()
+    }
+    
+    // MARK: - Rx
+    var disposeBag = DisposeBag()
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+
+        self.addViews()
+        self.view.setNeedsUpdateConstraints()
+    }
+
+    deinit {
+        logger.verbose("DEINIT: \(self.className)")
+    }
+
+    // MARK: - Layout Constraints
+    override func updateViewConstraints() {
+        if !self.didSetupConstraints {
+            self.setupConstraints()
+            self.didSetupConstraints = true
+        }
+        super.updateViewConstraints()
+    }
+
+    // MARK: - Configure
+    func addViews() { }
+
+    func setupConstraints() { }
+
+}
--- TodayMood/Sources/ViewControllers/ViewController.swift
+++ TodayMood/Sources/ViewControllers/ViewController.swift
@@ -8,20 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .red
-        
-        // Now let’s log!
-        log.verbose("not so important")  // prio 1, VERBOSE in silver
-        log.debug("something to debug")  // prio 2, DEBUG in green
-        log.info("a nice information")   // prio 3, INFO in blue
-        log.warning("oh no, that won’t be good")  // prio 4, WARNING in yellow
-        log.error("ouch, an error did occur!")  // prio 5, ERROR in red
     }
-
-
 }
-
