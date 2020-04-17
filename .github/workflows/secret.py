c66abc136066c104feb2263831aac512bf46655f
Fernando <fernando@kakao.com>
2020-04-17 20:49:43 +0900

    feat(config): Secerts.swift 동적 생성 로직 추가

---
+++ .github/workflows/secret.py
@@ -0,0 +1,10 @@
+source_dir = "../../TodayMood/Sources/"
+secret_file = "Secrets.swift"
+full_path = source_dir + secret_file
+secret_content = "struct Secrets {\n\tstatic let clientID = \"\"\n\tstatic let clientSecret = \"\"\n}"
+
+f = open(full_path,'w')
+f.write(secret_content)
+f.close()
+
+print("[+] Secrets.swift is Generated !")
