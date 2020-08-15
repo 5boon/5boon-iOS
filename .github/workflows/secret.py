41651c7a38c3025ecdeda5cc08dc991a15a90b90
Fernando <fernando@kakao.com>
2020-08-15 21:36:54 +0900

    fix(logic): str 형변환 추가

--- .github/workflows/secret.py
+++ .github/workflows/secret.py
@@ -7,7 +7,7 @@ client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 user_agent = os.getenv('USER_AGENT')
 
-secret_content = "struct Secrets {\n\tstatic let clientID = \""+client_id+"\"\n\tstatic let clientSecret = \""+client_secret+"\"\n\tstatic let userAgent = \""+user_agent+"\"\n}"
+secret_content = "struct Secrets {\n\tstatic let clientID = \""+client_id+"\"\n\tstatic let clientSecret = \""+client_secret+"\"\n\tstatic let userAgent = \""+str(user_agent)+"\"\n}"
 
 f = open(full_path,'w')
 f.write(secret_content)
